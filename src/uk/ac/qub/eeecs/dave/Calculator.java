faaa327c8268ac72ecf71f50be29212bb2eb19e8
teeradol123 <75262340+teeradol123@users.noreply.github.com>
2021-10-19 11:56:58 +0100

    Update Calculator.java

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -1,12 +1,21 @@
 package uk.ac.qub.eeecs.dave;
 
 public class Calculator {
+	
 	public int add(int a, int b) {
+		System.out.println("Hello");
 		return a+b+1;
+		
 	}
 	
 	public int sub(int a, int b) {
 		return a-b;
+		
 	} 
 	
+	
+	
 }
+
+
+
